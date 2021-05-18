a546d861bdd691ce595dd93fc9e5a27b1d1733fa
Fab <boolean@fabiopacifici.com>
2021-05-18 11:33:53 +0100

    Add live coding vue-hello

---
+++ vue-hello/app.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        classi: 'color_red bg_dark',
+        message: 'Hello VueJS',
+        /* marsImg: 'https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg', */
+        marsImg: './assets/img/mars.jpg'
+    },
+
+    methods:{
+
+    }
+
+})
