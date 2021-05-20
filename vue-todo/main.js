c9531ce24597dafdac3fec0c2c9233c1c9885a48
Fab <boolean@fabiopacifici.com>
2021-05-20 10:49:24 +0100

    Live vue-todo final

--- vue-todo/main.js
+++ vue-todo/main.js
@@ -1,27 +1,63 @@
 const app = new Vue({
-    
     el: '#app',
     data: {
         logo: './assets/img/logo.png' ,
         newTask: '',
+        error: '',
         tasks: [
             'Learn HTML',
             'Learn CSS',
             'Learn JS'
-        ]
+        ],
+        completedTasks: [],
+        trash: []
     },
     methods: {
        /** Add a task */
        addTask(){
-           // pushare new task in tasks
-            this.tasks.push(this.newTask)
+            if(this.newTask.length > 3) {
+            // pushare new task in tasks
+            this.tasks.unshift(this.newTask)
             this.newTask = ''
+            this.error = ''
+            } else {
+                this.error = 'Devi inserire almeno 3 caratteri'
+            }
        },
-       removeTask(index){
-        // remove task
-        console.log('Remove task n.' + index);
-        // Usare splice
-        this.tasks.splice(index, 1)
+       removeTask(index, task){
+            // remove task
+            console.log('Remove task n.' + index);
+    
+            // Usare splice per rimuovere la task
+            this.tasks.splice(index, 1)
+            this.trash.unshift(task)
+       },
+       updateTask(){
+           alert('Task updeted')
+       },
+       completeTask(index, task){
+            this.completedTasks.unshift(task)
+            this.tasks.splice(index, 1)
+       },
+
+       restoreCompletedTask(index, task){
+            // move the task inside the todo list
+            this.tasks.unshift(task)
+            // remove the task
+            this.completedTasks.splice(index, 1)
+       },
+       emptyTrash(){
+
+        const destroy = confirm('Are you sure you want to empty the trash?')
+
+        if(destroy){
+            this.trash = [];
+        }
+
+       },
+       restoreTrashedTasks(index, task){
+             this.tasks.unshift(task);
+             this.trash.splice(index, 1)
        }
     }
 });
